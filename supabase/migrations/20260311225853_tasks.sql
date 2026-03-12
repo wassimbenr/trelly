55dab1692cbc74f1b70a1a24e202ca76d0ea3fdf
wassimbenr <wassimbenr@gmail.com>
2026-03-12 11:56:32 +0100

    feat: add initial project structure and setup

---
+++ supabase/migrations/20260311225853_tasks.sql
@@ -0,0 +1,57 @@
+
+  create table "public"."tasks" (
+    "id" uuid not null default gen_random_uuid(),
+    "title" text not null,
+    "created_at" timestamp with time zone not null default now()
+      );
+
+
+alter table "public"."tasks" enable row level security;
+
+CREATE UNIQUE INDEX tasks_pkey ON public.tasks USING btree (id);
+
+alter table "public"."tasks" add constraint "tasks_pkey" PRIMARY KEY using index "tasks_pkey";
+
+grant delete on table "public"."tasks" to "anon";
+
+grant insert on table "public"."tasks" to "anon";
+
+grant references on table "public"."tasks" to "anon";
+
+grant select on table "public"."tasks" to "anon";
+
+grant trigger on table "public"."tasks" to "anon";
+
+grant truncate on table "public"."tasks" to "anon";
+
+grant update on table "public"."tasks" to "anon";
+
+grant delete on table "public"."tasks" to "authenticated";
+
+grant insert on table "public"."tasks" to "authenticated";
+
+grant references on table "public"."tasks" to "authenticated";
+
+grant select on table "public"."tasks" to "authenticated";
+
+grant trigger on table "public"."tasks" to "authenticated";
+
+grant truncate on table "public"."tasks" to "authenticated";
+
+grant update on table "public"."tasks" to "authenticated";
+
+grant delete on table "public"."tasks" to "service_role";
+
+grant insert on table "public"."tasks" to "service_role";
+
+grant references on table "public"."tasks" to "service_role";
+
+grant select on table "public"."tasks" to "service_role";
+
+grant trigger on table "public"."tasks" to "service_role";
+
+grant truncate on table "public"."tasks" to "service_role";
+
+grant update on table "public"."tasks" to "service_role";
+
+
